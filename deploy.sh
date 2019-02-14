2965bd38dd653b2d94b5d03dfbdb45f5a62adb3c
croissant <omelette@pomme.bona.jp>
2019-02-14 14:35:57 +0000

    update documents.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 cd `dirname $0`
-pipenv run mkdocs build
+pipenv run mkdocs build -f GPF-I.yml
+pipenv run mkdocs build -f CPF-D.yml
 cp css/* docs/css/
 git add . && git commit -m 'update documents.'
 git push origin master
